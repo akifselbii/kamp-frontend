541ea1806445277196ba74056c16b941c9ccd133
Mehmet Akif Selbi <akifselbii@gmail.com>
2021-11-07 01:27:30 +0300

    7.10.2021

---
+++ northwind/src/app/components/product/product.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  products:Product[] = [];
+  dataLoaded = false;
+  constructor(private protectedService:ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.protectedService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    });
+  }
+
+}
